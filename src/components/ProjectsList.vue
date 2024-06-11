b84fee0e550a091ac69e5aac5583cdf3faed86da
WilliamHaxhija <williamhaxhija@gmail.com>
2024-06-11 22:59:15 +0200

    add milestone 4

--- src/components/ProjectsList.vue
+++ src/components/ProjectsList.vue
@@ -1,6 +1,25 @@
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'ProjectsList'
+    name: 'ProjectsList',
+    data() {
+        return {
+            projects: null
+        }
+    },
+    methods: {
+        getProjects() {
+            axios.get('http://127.0.0.1:8000/api/projects')
+            .then((response) => {
+                console.log(response.data.results);
+                // this.projects = 
+            });
+        }
+    },
+    mounted() {
+        this.getProjects();
+    }
 }
 </script>
 
